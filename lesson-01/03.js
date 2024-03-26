e9cab095ae65de977d51944824cf663903b057f4
yurysukhan <yurysukhan@gmail.com>
2024-03-26 12:12:14 +0300

    second fix: postfix and prefix plus and minus signs

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,3 +1,6 @@
 let number = 1;
 
-console.log(`${number++},${++number},${number--},${--number}`);
+console.log(number++); 
+console.log(++number); 
+console.log(number--); 
+console.log(--number); 
