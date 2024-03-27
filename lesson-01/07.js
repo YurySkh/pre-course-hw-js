08cf9ab0d991b659659cb4a2645eb77ad8452d98
yurysukhan <yurysukhan@gmail.com>
2024-03-27 14:35:25 +0300

    toUpperCase (string method)

---
+++ lesson-01/07.js
@@ -0,0 +1,13 @@
+// Task №7
+
+// Дано: 
+// ​let iAm = 'я' 
+// let krasavchik = "красавчик!"; 
+
+// Выведи в консоль «Я красавчик!» («Я» с заглавной).
+
+// Solution:
+let iAm = 'я' 
+let krasavchik = "красавчик!"; 
+
+console.log(`${iAm.toUpperCase()} ${krasavchik}`);
